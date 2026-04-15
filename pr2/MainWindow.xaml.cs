5fa1f16a09816e2fef4658977eb24a99ab20d6a3
hubskyi-maksym <c.hubskyi.maksym@student.uzhnu.edu.ua>
2026-04-15 17:42:14 +0300

    Basic UI with buttons and text fields

--- pr2/MainWindow.xaml.cs
+++ pr2/MainWindow.xaml.cs
@@ -1,28 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace pr2
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BtnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            string name = TxtMuscleName.Text;
+            string group = TxtMuscleGroup.Text;
+
+            StatusText.Text = $"Виконується пошук: {name} ({group})";
+
+            MessageBox.Show($"Шукаємо м'яз:\nНазва: {name}\nГрупа: {group}",
+                            "Результат пошуку",
+                            MessageBoxButton.OK,
+                            MessageBoxImage.Information);
+        }
+
+        private void BtnClear_Click(object sender, RoutedEventArgs e)
+        {
+            TxtMuscleName.Clear();
+            TxtMuscleGroup.Clear();
+
+            StatusText.Text = "Поля пошуку очищено";
+        }
+
+        private void BtnDetails_Click(object sender, RoutedEventArgs e)
+        {
+            StatusText.Text = "Завантаження детальної інформації...";
+            MessageBox.Show("Тут відкриється нове вікно з моделлю та деталями.",
+                            "Деталі анатомії");
         }
     }
 }
